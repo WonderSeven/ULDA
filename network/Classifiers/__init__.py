acfefda1529eb2d32a47e3c0a75d7d79fcab4df4
SuiAn <suianxiaosheng@qq.com>
2022-04-23 11:40:11 +0800

    Update __init__.py

--- network/Classifiers/__init__.py
+++ network/Classifiers/__init__.py
@@ -1,5 +1,5 @@
 import sys
-import network.Classifiers.ProtoNet
+# import network.Classifiers.ProtoNet
 import network.Classifiers.RelationNet
 import network.Classifiers.MAML
 import network.Classifiers.Linear_classifier
